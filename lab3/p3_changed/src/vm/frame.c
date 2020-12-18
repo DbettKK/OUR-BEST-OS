c1a19ceb6f0bafcad50bdcda2fdb91544a7aeb49
Colanns <mycolands@gmail.com>
2020-12-19 02:59:50 +0800

    Update frame.c

--- lab3/p3_changed/src/vm/frame.c
+++ lab3/p3_changed/src/vm/frame.c
@@ -18,17 +18,16 @@ static size_t hand;
 void
 frame_init (void) 
 {
-  void *base;
-
   lock_init (&scan_lock);
   
   frames = malloc (sizeof *frames * init_ram_pages);
 
-  while ((base = palloc_get_page (PAL_USER)) != NULL) 
+  while ( palloc_get_page (PAL_USER) != NULL ) 
     {
-      struct frame *f = &frames[frame_cnt++];
+      frame_cnt++;
+      struct frame *f = &frames[frame_cnt];
       lock_init (&f->lock);
-      f->base = base;
+      f->base = palloc_get_page (PAL_USER);
       f->page = NULL;
     }
 }
--- lab3/p3_changed/src/vm/frame.h
+++ lab3/p3_changed/src/vm/frame.h
@@ -20,4 +20,4 @@ void frame_lock (struct page *);
 void frame_free (struct frame *);
 void frame_unlock (struct frame *);
 
-#endif /* vm/frame.h */
+#endif
