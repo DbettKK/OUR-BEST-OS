470d751960e324a34e67be86ec8c46e5659bb21a
Colanns <mycolands@gmail.com>
2020-12-20 16:49:26 +0800

    All 113 tests passed

--- lab3/pintos3_all_passed/pintos/src/userprog/process.c
+++ lab3/pintos3_all_passed/pintos/src/userprog/process.c
@@ -668,7 +668,7 @@ setup_stack (const char *cmd_line, void **esp)
     page->frame = frame_alloc_and_lock (page);
     if (page->frame != NULL) {
       bool result;
-      page->read_only = false;
+      page->r_only = false;
       page->private = false;
       result = map_to_user (page->frame->base, page->addr, cmd_line, esp);
       frame_unlock (page->frame);
--- lab3/pintos3_all_passed/pintos/src/vm/page.h
+++ lab3/pintos3_all_passed/pintos/src/vm/page.h
@@ -3,48 +3,38 @@
 
 #include <hash.h>
 #include "devices/block.h"
-#include "filesys/off_t.h"
 #include "threads/synch.h"
+#include "filesys/off_t.h"
 
-/* Virtual page. */
 struct page 
   {
-    /* Immutable members. */
-    void *addr;                 /* User virtual address. */
-    bool read_only;             /* Read-only page? */
-    struct thread *thread;      /* Owning thread. */
+    struct thread *thread;
+    void *addr;
+    bool r_only;
+    bool dirty;
+    struct hash_elem elem;
+    struct frame *frame;
 
-    /* Accessed only in owning process context. */
-    struct hash_elem hash_elem; /* struct thread `pages' hash element. */
-
-    /* Set only in owning process context with frame->frame_lock held.
-       Cleared only with scan_lock and frame->frame_lock held. */
-    struct frame *frame;        /* Page frame. */
-
-    /* Swap information, protected by frame->frame_lock. */
-    block_sector_t sector;       /* Starting sector of swap area, or -1. */
+    block_sector_t sector;
     
-    /* Memory-mapped file information, protected by frame->frame_lock. */
-    bool private;               /* False to write back to file,
-                                   true to write back to swap. */
-    struct file *file;          /* File. */
-    off_t file_offset;          /* Offset in file. */
-    off_t file_bytes;           /* Bytes to read/write, 1...PGSIZE. */
+    bool private;
+    struct file *file;
+    off_t file_offset;
+    size_t file_bytes;
   };
 
+bool page_lock (const void *, bool w_write);
+bool page_unlock (const void *);
 void page_exit (void);
-
-struct page *page_allocate (void *, bool read_only);
+void page_destroy (struct hash_elem *);
+struct page *get_page_with_addr (const void *);
+struct page *page_allocate (void *, bool r_only);
 void page_deallocate (void *vaddr);
-
 bool page_in (void *fault_addr);
 bool page_out (struct page *);
-bool page_accessed_recently (struct page *);
-
-bool page_lock (const void *, bool will_write);
-void page_unlock (const void *);
+bool is_page_accessed (struct page *);
 
 hash_hash_func page_hash;
 hash_less_func page_less;
 
-#endif /* vm/page.h */
+#endif
